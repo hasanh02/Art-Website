86e624d4abd4728b8d1c89eedc95566201d0e1b4
hasan <mmoosa3@uwo.ca>
2024-05-29 15:46:10 -0400

    Routes

--- art/src/client/App.js
+++ art/src/client/App.js
@@ -1,6 +1,22 @@
+import {Router, Routes, Route} from 'react-router-dom'
+import LandingPage from '.components/LandingPage'
+import Home from '.components/Home'
+import About from '.components/About'
+import Shop from '.components/Shop'
+import Contact from '.components/Contact'
+
 function App(){
     return(
-        <h1>Hi im starting</h1>
+        
+        <Router>
+            <Routes>
+                <Route path='/' element={<LandingPage/>}/>
+                <Route path='/Home' element={<Home/>}/>
+                <Route path='/About' element={<About/>}/>
+                <Route path='/Shop' element={<Shop/>}/>
+                <Route path='/Contact' element={<Contact/>}/>
+            </Routes>
+        </Router>
     )
 }
 export default App;
