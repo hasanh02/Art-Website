d62223d7b86c56c7f3b9fcc3858a41abd1bb50c7
hasan <mmoosa3@uwo.ca>
2024-05-29 16:18:20 -0400

    testing pages

--- art/src/App.js
+++ art/src/App.js
@@ -1,9 +1,9 @@
 import {Router, Routes, Route} from 'react-router-dom'
-import LandingPage from '.components/LandingPage'
-import Home from '.components/Home'
-import About from '.components/About'
-import Shop from '.components/Shop'
-import Contact from '.components/Contact'
+import LandingPage from './components/LandingPage'
+import Home from './components/Home'
+import About from './components/About'
+import Shop from './components/Shop'
+import Contact from './components/Contact'
 
 function App(){
     return(
---
+++ art/src/components/LandingPage.js
@@ -0,0 +1,6 @@
+function LandingPage(){
+    return(
+        <h1>LandingPage</h1>
+    )
+}
+export default LandingPage;
